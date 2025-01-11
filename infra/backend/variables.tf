6ed5dcc336785e1981070708460627c32e94a84b
Santiago Fuentes Rios <sjfuentes@MacBook-Pro-de-Santiago.local>
2025-01-10 20:40:10 -0500

    add infra backend configuration

---
+++ infra/backend/variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  type    = string
+  default = "sjfuentes"
+}
+
+variable "repo_name" {
+  type    = string
+  default = "interview-prep"
+}
+
+variable "region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "account_id" {
+  type    = string
+  default = "882567899411"
+}
